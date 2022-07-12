bfed61373efe815b087ed4a42c165cafef1d7d3e
zhandosalmaty <42071733+zhandosalmaty@users.noreply.github.com>
2022-07-12 13:50:14 +0600

    Update sent.rb

--- app/controllers/sent.rb
+++ app/controllers/sent.rb
@@ -1,11 +1,18 @@
+class MyTele2::QueryController < ::ApplicationController
+
+
 require 'telegram/bot'
 
+  
+  
+def index
 token = '62edb442efbad15ad8f5d2226ccfa8c8'
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-
   end
+end
+end
+
 end
